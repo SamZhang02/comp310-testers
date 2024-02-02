b3da142c41be8cc4f0bc5a8003bef62c1c12ea2c
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-02-01 21:54:51 -0500

    add tests

--- a1/tests.py
+++ a1/tests.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import shutil
 
 class ShellTestClient:
 
@@ -34,21 +35,27 @@ class ShellTestClient:
     stdout_lines = self.stdout.strip().split('\n')
     expected_lines = expected_output.strip().split('\n')
 
-    # print(stdout_lines)
-    # print(expected_lines)
+    # print(self.stdout)
+    # print(expected_output)
+
+    i = 0
+    if len(stdout_lines) != len(expected_lines):
+      print(f"Different numbers of lines, {len(stdout_lines)} in stdout, {len(expected_lines)} expected")
+      return False
 
     for stdout_line, expected_line in zip(stdout_lines, expected_lines):
       if stdout_line.strip() != expected_line.strip():
-        print(f"Test failed for line: {stdout_line}")
+        print(f"Test failed for line: {i}")
         print(f"Expected: {expected_line}")
         print(f"Got: {stdout_line}")
         return False
+      i += 1
 
     return True
 
 
-COMMANDS_PATH = "./commands"
-OUTPUT_PATH = "./outputs"
+COMMANDS_PATH = "commands"
+OUTPUT_PATH = "outputs"
 
 def run_test(name, input_file, output_file):
     client = ShellTestClient('myshell')
@@ -62,10 +69,42 @@ def run_test(name, input_file, output_file):
         print(f"{name} ------ \033[92mok\033[0m")
         return True
 
+
+def clean_directories():
+    assets_dir = "assets"
+    keep_dir = 'keep'
+
+    os.chdir(assets_dir)
+
+    for item in os.listdir('.'):
+        path = os.path.join('.', item)
+
+        if item == keep_dir:
+            continue
+
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+
+    os.chdir('..')
+
+
+
 def main():
   tests: dict[str, tuple[str,str]] = {
-    "Should launch without error": ("launch.txt", "launch.txt"),
-    "Should set variables like the assignment instructions": ("set_1.txt", "set_1.txt")
+    "Launch without error": ("launch.txt", "launch.txt"),
+    "Set variables like the assignment instructions": ("set_1.txt", "set_1.txt"),
+    "Error when setting empty variables and set number as variables": ("set_2.txt", "set_2.txt"),
+    "Echo non-variable, empty variables, filled variables, numeric variable names": ("echo.txt", "echo.txt"),
+    "mkdir empty, mkdir too many, mkdir new dir. touch 2 new files and ls": ("navigate_1.txt", "navigate_1.txt"),
+    "navigate to existing directory, ls existing files": ("navigate_2.txt", "navigate_2.txt"),
+    "nested cd": ("navigate_3.txt", "navigate_3.txt"),
+    "empty_command": ("empty.txt", "empty.txt"),
+    "nested mkdir": ("nest_mkdir.txt", "nest_mkdir.txt"),
+    "nested touch, empty touch": ("touch.txt", "touch.txt"),
+    "null cat, empty cat, long cat": ("meow.txt", "meow.txt"),
+    "empty ls, normal ls": ("ls.txt", "ls.txt")
   }
 
   ok_count = 0
@@ -75,6 +114,8 @@ def main():
 
     if run_test(name, input, output):
       ok_count += 1
+
+    clean_directories()
 
   print(f"{len(tests)} tests ran, {ok_count} tests passed.")
 
